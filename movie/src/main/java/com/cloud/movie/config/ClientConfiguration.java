9e40af607766aa1531bbdd1c658b49178c29766b
dyangx <yaabbccj@163.com>
2019-10-20 18:20:29 +0800

    配置负载均衡策略

---
+++ movie/src/main/java/com/cloud/movie/config/ClientConfiguration.java
@@ -0,0 +1,9 @@
+package com.cloud.movie.config;
+
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RibbonClient(name = "micro-provider-movie",configuration = RibbonClientConfiguration.class)
+public class ClientConfiguration {
+}
---
+++ movie/src/main/java/com/cloud/movie/config/RibbonClientConfiguration.java
@@ -0,0 +1,24 @@
+package com.cloud.movie.config;
+
+import com.netflix.loadbalancer.BestAvailableRule;
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 负载均衡配置
+ */
+@Configuration
+public class RibbonClientConfiguration {
+
+    @Bean
+    public IRule ribbonRuble(){
+
+        //随机
+        return new RandomRule();
+
+        //选择一个最小的并发请求的server
+//        return new BestAvailableRule();
+    }
+}
