c9b8c26cb5aeb2319c8e72dc743bf562e6c50778
dyangx <yaabbccj@163.com>
2019-10-27 21:45:58 +0800

    初步整合zuul网关

--- turbine/src/main/java/com/cloud/turbine/TurbineApplication.java
+++ turbine/src/main/java/com/cloud/turbine/TurbineApplication.java
@@ -2,8 +2,10 @@ package com.cloud.turbine;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
 @SpringBootApplication
+@EnableTurbine
 public class TurbineApplication {
 
     public static void main(String[] args) {
--- user/src/main/java/com/cloud/user/UserApplication.java
+++ user/src/main/java/com/cloud/user/UserApplication.java
@@ -2,6 +2,7 @@ package com.cloud.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
@@ -12,7 +13,8 @@ import org.springframework.web.client.RestTemplate;
 @EnableDiscoveryClient
 @SpringBootApplication
 @EnableFeignClients
-@EnableHystrix
+//@EnableHystrix
+//@EnableCircuitBreaker
 public class UserApplication {
 
     @Bean
