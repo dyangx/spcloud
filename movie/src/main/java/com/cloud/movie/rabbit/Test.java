ca1a69447f04f1f58093694e7d280b06135bd6b3
yangjie <yangjie@yangjie@rivamed.cn>
2019-11-20 15:19:04 +0800

    commit

---
+++ movie/src/main/java/com/cloud/movie/rabbit/Test.java
@@ -0,0 +1,42 @@
+package com.cloud.movie.rabbit;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * @author: yangjie
+ * @date: Created in 2019/11/20 13:53
+ */
+public class Test {
+
+    private static final String QUEUE_NAME = "hello_rabbit";
+
+    private static final boolean DURABLE = false;
+
+    //需要发送的消息列表
+    public static final String[] msgs = {"task 1", "task 2", "task 3", "task 4", "task 5", "task 6"};
+
+    public static void main(String[] args) throws IOException, TimeoutException {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("127.0.0.1");
+        Connection connection = null;
+        Channel channel = null;
+        connection = factory.newConnection();
+        channel = connection.createChannel();
+
+        // queue
+        channel.queueDeclare(QUEUE_NAME,DURABLE,false,false,null);
+
+        //推送消息
+        for (String s : msgs){
+            channel.basicPublish("",QUEUE_NAME,null,s.getBytes());
+            System.out.println("已推送消息："+s);
+        }
+        channel.close();
+        connection.close();
+    }
+}
