037e4f37ae4e8b8849f73aabe1ef753dce855870
yangjie <yangjie@yangjie@rivamed.cn>
2020-03-19 16:08:11 +0800

    “zc

---
+++ user/src/test/java/jnuitTest/Test.java
@@ -0,0 +1,15 @@
+package jnuitTest;
+
+import reactor.core.publisher.Flux;
+
+/**
+ * @author: yangjie
+ * @date: Created in 2020/3/19 14:40
+ */
+public class Test {
+
+    @org.junit.Test
+    public void test(){
+        Flux.range(1, 10).map(x -> x*x).subscribe(System.out::print);
+    }
+}
