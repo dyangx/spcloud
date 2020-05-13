233b812af57c194be8378c06b66e6e59bc379191
yangjie <yangjie@yangjie@rivamed.cn>
2020-05-13 16:27:22 +0800

    zk

---
+++ zk/src/test/java/com/cloud/zk/ZkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloud.zk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
